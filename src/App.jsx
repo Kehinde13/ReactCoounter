8d1280f91840998083a5b02c87609b5cf013cfc2
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-09-06 03:26:03 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import './App.css'
+import {
+  ChevronDown,
+  ChevronsDown,
+  ChevronUp,
+  ChevronsUp,
+  Hash,
+  RotateCcw
+} from 'react-feather'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(current => current += 1)
+  }
+
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(current => current -= 1)
+    }
+  }
+
+  const reset = () => {
+    setCount(current => current = 0)
+  }
+
+  const randomCount = () => {
+    setCount(current =>
+      current = Math.floor(Math.random() * 100)
+    )
+  }
+
+  const decrementCountBy10 = () => {
+    if (count > 0) {
+     setCount(current => current -= 10)
+    }
+  }
+
+  const incrementCountBy10 = () => {
+    setCount(current => current += 10)
+  }
+
+  return (
+    <div className='container'>
+      <h3>Counter</h3>
+      <h1>{count}</h1>
+      <div className="buttons">
+        <button onClick={incrementCount}>
+          <ChevronUp />
+        </button>
+        <button onClick={incrementCountBy10}>
+          <ChevronsUp />
+        </button>
+        <button onClick={reset}>
+          <RotateCcw />
+        </button>
+        <button onClick={randomCount}>
+          <Hash />
+        </button>
+        <button onClick={decrementCountBy10}>
+          <ChevronsDown />
+        </button>
+        <button onClick={decrementCount}>
+          <ChevronDown />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
